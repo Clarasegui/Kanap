8a337f0c543caf46e9addf8139377ce4c78ae7fe
Clara Segui <clarasegui@macbook-air.home>
2022-12-15 21:47:50 +0100

    Product page

--- front/js/product.js
+++ front/js/product.js
@@ -1,33 +1,56 @@
 //------------------------------------------------------------------------
-// Récupération du produit par l'id produit dans l'URL
+// Récupération et intégration du produit par l'id produit dans l'URL 
 //------------------------------------------------------------------------
 
 const getIdByUrl = new URLSearchParams(document.location.search);
 const id = getIdByUrl.get('_id'); // Récupère l'id produit dans l'URL
 
-let fetchPromise = fetch(`http://localhost:3000/api/products/${id}`);
-
-// Promise<Response>.then => Response
-
-fetch(`http://localhost:3000/api/products/${id}`) // Promise<Response>
-    .then((response) => response.json()) // Promise<Json>
-    .then((product) => {
-        document.getElementById("description").innerText = product.description;
+fetch(`http://localhost:3000/api/products/${id}`) // Récupération du produit par l'id
+    .then((response) => response.json())
+    .then((product) => { // Intégre les données dans le DOM
         document.getElementById("title").innerText = product.name;
+        document.getElementById("description").innerText = product.description;
         document.getElementById("price").innerText = product.price;
-        document.getElementById("quantity").innerText = product.quantity;
-        document.getElementsByClassName("item__img")[0].innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}">`;
-
-        for (let color of product.colors) {
-            document.getElementById("colors").innerHTML += `<option value="${color}">${color}</option>`
+        document.getElementsByClassName("item__img")[0].innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}"></img>`;
+        for (let color of product.colors) { // Intégre les couleurs dans l'élément select
+            document.getElementById("colors").innerHTML += `<option value="${color}">${color}</option>`;
         }
-        
     });
 
 //------------------------------------------------------------------------
-// 
+// Récupération des données quantité et couleur
 //------------------------------------------------------------------------
 
+document.getElementById("addToCart").onclick = function () { // Ajoute la fonction onclick à l'élément button
+    let quantityOfProduct = parseInt(document.getElementById("quantity").value); // Récupère la valeur quantité
+    let colorOfProduct = document.getElementById("colors").value; // Récupère la valeur couleur
 
+    let currentCart = localStorage.getItem("cart"); // Crée le panier
 
+    if (currentCart === null) { // Si le panier est vide
+        let products = Array({ // Créer un tableau contenant les valeurs id, couleur et quantité
+            "id": id, "color": colorOfProduct, "quantity": quantityOfProduct
+        })
+        let productsJson = JSON.stringify(products); // Convertir la valeur en chaîne Json
+        localStorage.setItem("cart", productsJson); // Stocker dans le localStorage
+    } else {
+        let currentProducts = JSON.parse(currentCart); // Sinon récupérer le panier en parsant le Json pour récupérer l'objet Javascript
+        
+        matchedProductInCart = currentProducts.find(p => p.id === id && p.color === colorOfProduct);
+        if (matchedProductInCart != undefined) { // Si le panier contient un objet qui a le même id et la même couleur
+            matchedProductInCart.quantity += quantityOfProduct;
+            localStorage.setItem("cart",JSON.stringify(currentProducts));
+        } else {
+            let product = {
+                "id": id,
+                "color": colorOfProduct,
+                "quantity": quantityOfProduct
+            }
+            currentProducts.push(product);
+            let productsJson = JSON.stringify(currentProducts);
+            localStorage.setItem("cart", productsJson);
+        }
+
+    }
+}
 
--- front/js/script.js
+++ front/js/script.js
@@ -5,17 +5,13 @@
 async function loadProductsToHtml() {
     const response = await fetch("http://localhost:3000/api/products"); // Attend la réponse de l'API
     const products = await response.json(); // Traduit la réponse en json
-    let productsListHtmlSection = document.querySelector("#items"); // Crée la variable productsListHtmlSection qui stocke les produits dans la section "items"
     for (let product of products) {
-        const productCard = `<a href="./product.html?_id=${product._id}">
+        document.getElementById("items").innerHTML += `<a href="./product.html?_id=${product._id}">
         <article>
-            <img src="${product.imageUrl}" alt="${product.altTxt}">
-            <h3 class="productName">${product.name}</h3>
-            <p class="productDescription">${product.description}</p>
-        </article>
-        </a>` // Intègre les informations produit dans le code HTML
-        console.log(productCard); // Imprime dans la console le contenu de la constante "productCard"
-        productsListHtmlSection.innerHTML += productCard;
+          <img src="${product.imageUrl}" alt="${product.altTxt}">
+          <h3 class="productName">${product.name}</h3>
+          <p class="productDescription">${product.description}</p>
+        </article>`
     }
 }
 
